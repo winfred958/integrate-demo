e17b9f40f60b9c3a692f6e50f7a2c64440eefe97
winfred958 <winfred958@gmail.com>
2023-11-25 02:36:03 +0800

    feat: init object

---
+++ gateway/src/main/java/com/winfred/integrate/gateway/GatewayApp.java
@@ -0,0 +1,17 @@
+package com.winfred.integrate.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableWebFlux
+@EnableDiscoveryClient
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(GatewayApp.class);
+        springApplication.run(args);
+    }
+}
